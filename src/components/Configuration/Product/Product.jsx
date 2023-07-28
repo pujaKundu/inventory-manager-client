a2b03118759de8bfc85a8655742989a5d291e38d
pujaKundu <iampujakundu@gmail.com>
2023-07-28 22:12:16 +0600

    delete product

--- src/components/Configuration/Product/Product.jsx
+++ src/components/Configuration/Product/Product.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { TableCell, TableRow } from "@mui/material";
 import "./Product.scss";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useDeleteProductMutation } from "../../../features/products/productsApi";
 
 const Product = ({ product }) => {
-  const { id,name, price, totalSales, stock, totalOrder,category } = product;
+  const { id, name, price, totalSales, stock, totalOrder, category } = product || {};
+  const [deleteProduct]=useDeleteProductMutation()
+  const handleDelete = (e) => {
+    e.preventDefault();
+deleteProduct(id)
+  }
   return (
     <>
       <TableRow>
@@ -28,15 +36,12 @@ const Product = ({ product }) => {
         <TableCell align="left" className="cell">
           {totalOrder}
         </TableCell>
-        
+
         <TableCell align="left" className="cell">
-          view
+          <EditIcon />
         </TableCell>
         <TableCell align="left" className="cell">
-          edit
-        </TableCell>
-        <TableCell align="left" className="cell">
-          delete
+          <DeleteIcon onClick={handleDelete} />
         </TableCell>
       </TableRow>
     </>
